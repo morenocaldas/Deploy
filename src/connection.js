5646ae370d0ab5b89daafecb70f7e9ae617f9b98
morenocaldas <morenocaldas@gmail.com>
2022-08-23 19:00:04 -0300

    Mudanças no arquivo de conexão

--- src/connection.js
+++ src/connection.js
@@ -1,12 +1,12 @@
 const knex = require("knex")({
-    client: 'pg',
+    client: process.env.DB_CLIENT,
 
     connection: {
-        user: 'xmvkswcisyrfvi',
-        host: 'ec2-3-213-228-206.compute-1.amazonaws.com',
-        database: 'd535si3t184cl8',
-        password: '04602782b35839ef498626058b5a1970b9df0e242a7f1b37b53bb1388ce99b1e',
-        port: '5432',
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_DATABASE,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT,
         ssl: {
             rejectUnauthorized: false
         }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
-require('dotenv').config();
 
 const app = express();
 
