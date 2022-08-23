d57449012aa806a3e09f3d813dfdea345a040620
morenocaldas <morenocaldas@gmail.com>
2022-08-23 16:13:09 -0300

    Inserida a porta no arquivo de conexão

--- src/connection.js
+++ src/connection.js
@@ -6,6 +6,7 @@ const knex = require("knex")({
         host: 'ec2-3-213-228-206.compute-1.amazonaws.com',
         database: 'd535si3t184cl8',
         password: '04602782b35839ef498626058b5a1970b9df0e242a7f1b37b53bb1388ce99b1e',
+        port: '5432',
         ssl: {
             rejectUnauthorized: false
         }
